c8dc3ca4a5f6dee886c2ca5a1cc25fda63aab7f1
Raghav Sharma <irraghav7@gmail.com>
2020-02-10 07:48:44 -0500

    To test if having the repository commit will work from the home windows machine

--- src/components/layouts/styleguide.js
+++ src/components/layouts/styleguide.js
@@ -6,7 +6,9 @@ import "../../styles/main.css"
 export default ({ children }) => (
   <div id="styleguide">
     <header className={styles.header}>
+
         <h1 className={styles.h1}> Styleguide for <i>raghavsharma.info</i></h1>
+        <h2> Documenting the work being around styling my website</h2>
         <div className={styles.menuSelector}>
             
         </div>
